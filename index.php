a1fb968b5ea0ada044df51471999b448ddb150ec
Rimma1984 <rimmakapaweva@gmail.com>
2023-03-12 13:07:43 +0600

    1203

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang='ru'>
+
+<body>
+   <form action="script.php" method="GET">
+        <input type="text" name="username" placeholder="Username">
+        <button>Send</button>
+    </form>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,32 @@
+<pre> 
+<?php
+// echo "Hello, {$_GET['username']}";
+
+// switch ($_GET['username'] ) {
+//     case "admin":
+//     case "moderator":
+//     case "accsess":
+//         echo 'Good!';
+//         break;
+//         default: 
+//         echo 'Warning!';
+//         break;
+// }
+   
+// $i = 0;
+
+// while ($i < 100) {
+//     echo $i . "\n";
+//     $i++;
+// }
+
+// for ($i = 0; $i < 25; $i++) {
+//     echo $i . "\n";
+// }
+
+$array = ["a", "b", "c", "d", "name" => "John"];
+
+for ($i = 0; $i > count(($array); $i++))
+
+?>
+</pre>
